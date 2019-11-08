ee0f5e691f1fd9c79f1c1c10298145e26ac7e99e
VeselinovStf <veselinovStf@gmail.com>
2019-11-08 07:04:33 +0200

    Static Contact page done

--- src/Web/Controllers/HomeController.cs
+++ src/Web/Controllers/HomeController.cs
@@ -29,6 +29,10 @@ namespace Web.Controllers
         {
             return View();
         }
+        public IActionResult Contact()
+        {
+            return View();
+        }
 
         public IActionResult Privacy()
         {
